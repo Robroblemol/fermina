c7e061ea3ccea2cdef31831b9f43c54c4f429d28
Raziel <robroblemol>
2020-05-04 11:57:24 -0500

    button add added

--- app/screens/Letter/index.js
+++ app/screens/Letter/index.js
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
-import { Text, FlatList, View, TouchableHighlight, StyleSheet, Alert} from 'react-native';
+import { Text, FlatList, View, TouchableOpacity, StyleSheet, Alert} from 'react-native';
 import { Actions } from 'react-native-router-flux'
+import  Icon  from 'react-native-vector-icons/dist/FontAwesome';
 import { map } from 'ramda'
 import ListWriting from '../../components/ListWriting'
+import ModalCreateWriting from '../../components/ModalCreateWriting'
 
 const Letter = (letters) => {
+    const [ showModal, setShowModal ]=useState(false);
     console.log('Letter!!!!!!!!!!!!!!!11');
     
     console.log(letters);
@@ -23,7 +26,7 @@ const Letter = (letters) => {
     }
     
     return (
-        <View style={style.container}>
+        <View style={styles.container}>
             <FlatList
                 data = {letters.data}
                 renderItem = {({item}) => (
@@ -36,16 +39,40 @@ const Letter = (letters) => {
                     />
                   )}
             />
-
+        <TouchableOpacity 
+            style={styles.btnAdd}
+            onPress={()=>setShowModal(true)}
+        >
+            <Icon
+            name= {'plus'} 
+            size= {24} 
+            color={'chocolate'} 
+                />
+        </TouchableOpacity>
+        <ModalCreateWriting
+            setIsVisible={setShowModal}
+            isVisible={showModal}
+        />
         </View>
     )
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
       flex: 1, 
         
     },
+    btnAdd: {
+        position: 'absolute',
+        width: 50,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        right: 30,
+        bottom: 30,
+        backgroundColor:'#FCAC17',
+        borderRadius: 45,
+      },
   })
 
 export default Letter;
